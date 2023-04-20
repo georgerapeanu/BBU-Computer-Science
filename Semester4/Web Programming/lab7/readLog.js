2ce37313bef544121f560470b74f4adf2332a0c1
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-20 14:31:47 +0300

    adds read single for logs

---
+++ Semester4/Web Programming/lab7/readLog.js
@@ -0,0 +1,10 @@
+$(() => {
+    id = parseInt(new URLSearchParams(window.location.search.substring(1)).get("id"));
+    $('.heading').text("Read log #" + id);
+    $.getJSON("readLog.php", {id:id}, json => {
+        $('.input_form__severity').val(json[0]["severity"]);
+        $('.input_form__date').val(json[0]["logDate"]);
+        $('.input_form__user').val(json[0]["user"]);
+        $('.input_form__text').val(json[0]["data"]);
+    })
+});
---
+++ Semester4/Web Programming/lab7/readLog.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("utils/connect.php");
+
+    if(!is_numeric($_GET["id"])) {
+        header("HTTP/1.1 400 Bad Request");
+        return ;
+    }
+    $id = (int)$_GET["id"];
+
+    $sql_query = "SELECT * FROM Logs WHERE id=?";
+    $stmt = mysqli_prepare($connection, $sql_query);
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    $rows = array();
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+    
+    $stmt->close();
+    $connection->close();
+
+    echo json_encode($rows);
+?>
